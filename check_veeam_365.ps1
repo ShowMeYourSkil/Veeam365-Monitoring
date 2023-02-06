23d69bbb2b6816c94d5b6013f51ad7722158851e
David Franzen <39274150+ShowMeYourSkil@users.noreply.github.com>
2023-02-06 19:02:11 +0100

    Update check_veeam_365.ps1
    Translate to English and Fix some error in the Foreach

--- check_veeam_365.ps1
+++ check_veeam_365.ps1
@@ -1,19 +1,19 @@
 <#
-    Ein Powershell Skript, was den Backup Status von Veeam Backup for Microsoft 365 prüfen
-    Es prüft, ob der Backup Job im Status Failed, Warning, Running, Success oder Stopped ist
+    A Powershell script that checks the backup status of Veeam Backup for Microsoft 365.
 
-    0 – Dienst ist OK.
-    1 – Dienst hat eine WARNUNG.
-    2 – Der Dienst befindet sich in einem KRITISCHEN Status.
-    3 – Der Dienst befindet sich im Status UNBEKANNT.
+    0 Service state is OK.
+    1 Service state is WARNING.
+    2 Service state is CRITICAL.
+    3 Servuce state is UNKNOWN.
 
     Author David Franzen
     Copyright (c) David Franzen
+    Version: v1.2
 #>
 
-#$output_jobs_failed_counter = 0;
-#$output_jobs_warning_counter = 0;
-#$output_jobs_success_counter = 0;
+$output_jobs_failed_counter = 0;
+$output_jobs_warning_counter = 0;
+$output_jobs_success_counter = 0;
 
 $jobs = Get-VBOJob
 $lastStatus = $job.LastStatus
@@ -21,38 +21,34 @@ $lastRun = $job.LasRun
 #$organization = $job.Organization
 $backupname = $job.name
 
-ForEach($jobisenabled in $jobs){
-    $isenabled = $jobs.IsEnabled
-
-    if ($isenabled -eq "False") {
-        Write-Host "Das Backup ist" $backupname "aktiviert";
-        exit 1;
-    }elseif($isenabled -eq "True"){
-        Write-Host "Das Backup ist" $backupname "deaktiviert";
-        exit 0;
-        }
-    }
+$VeeamModulePath = "C:\Program Files\Veeam\Backup365\Veeam.Archiver.PowerShell"
+$env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)$VeeamModulePath"
+Import-Module Veeam.Archiver.PowerShell
 
 ForEach($job in $jobs)
 {
-    if($lastStatus -eq "Running"){
-        Write-Host "Das Veeam 365 Backup" $backupname "läuft derzeit noch";
-        #$output_jobs_success_counter++;
+    $lastStatus = $job.LastStatus
+    $isenabled = $jobs.IsEnabled
+    if ($isenabled -eq "True") {
+        Write-Host "Das Backup ist" $backupname "deaktiviert";
+        exit 1;
+    }elseif($lastStatus -eq "Running"){
+        Write-Host "The BackupJob $backupname running"
         exit 0;
     }
     elseif($lastStatus -eq "Failed")
     {
-        Write-Host "Das Veeam 365 Backup" $backupname "ist erfolgreich gelaufen";
-        #$output_jobs_failed_counter++;
+        Write-Host "The Backupjob $backupname is in critical state";
+        $output_jobs_failed_counter++;
         exit 2;
     } elseif($lastStatus -eq "Warning"){
-        Write-Host "Das Veeam 365 Backup" $backupname "ist mit einer Warnung geendet";
+        Write-Host "The Backupjob $backupname is in critical state";
         $output_jobs_warning_counter++;
         exit 1;
     } elseif($lastStatus -eq "Success")
     {
-        Write-Host "Das Veeam 365 Backup" $backupname "ist erfolgreich gelaufen!";
-        #$output_jobs_success_counter++;
+        Write-Host "The Backupjob $backupname has run successfully";
+        $output_jobs_success_counter++;
         exit 0;
     }
 }
