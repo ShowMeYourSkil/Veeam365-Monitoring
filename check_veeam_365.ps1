fc9da909cfb01f25680149db5d46b49ea5e24fbb
ShowMeYourSkil <davidfranzen@t-online.de>
2023-10-11 15:51:59 +0200

    Update Contibutors

--- check_veeam_365.ps1
+++ check_veeam_365.ps1
@@ -9,6 +9,9 @@
     Author David Franzen
     Copyright (c) David Franzen
     Version: v1.3
+	
+	Contributor:
+	log1-c
 
     Tested on: Veeam Backup for Microsoft 365 V6
 #>
@@ -25,7 +28,7 @@ $output_jobs_running_counter = 0
 $output_jobs_disabled_counter = 0
 
 #Imports the Veeam365 Powershell module
-$VeeamModulePath = "C:\Program Files\Veeam\Backup365\Veeam.Archiver.PowerShell"
+$VeeamModulePath = 'C:\Program Files\Veeam\Backup365\Veeam.Archiver.PowerShell'
 $env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)$VeeamModulePath"
 Import-Module Veeam.Archiver.PowerShell
 
@@ -49,20 +52,20 @@ ForEach($job in $jobs)
     $backupname = $job.name
     #$runtime = $lastRun.CreationTime.toString("dd.MM.yyyy")
 
-    if($lastStatus -eq "Running"){
-        Write-Host "Running:" $backupname
+    if($lastStatus -eq 'Running'){
+        Write-Host 'Running:' $backupname
         $exitcode = $EXIT_OK
     }
-    elseif($lastStatus -eq "Failed")
+    elseif($lastStatus -eq 'Failed')
     {
         $critical_jobs += "Critical: $backupname`n"
         $output_jobs_failed_counter++
         $exitcode = $EXIT_CRITICAL
-    } elseif($lastStatus -eq "Warning"){
+    } elseif($lastStatus -eq 'Warning'){
         $warning_jobs += "Warning: $backupname`n"
         $output_jobs_warning_counter++
         $exitcode = $EXIT_WARNING
-    } elseif($lastStatus -eq "Success")
+    } elseif($lastStatus -eq 'Success')
     {
         #Write-Host "Success:" $backupname
         $output_jobs_success_counter++
@@ -84,7 +87,6 @@ ForEach($license in $licenses)
     # Veeam allows an over-utilization of licenses of 10%.
     if($licenseLifetime){
         $license_status = "OK: License is $licensestatus"
-<<<<<<< HEAD
         $output_jobs_success_counter++
         $exitcode = $EXIT_OK
         if($usedLicenses -gt $totalLicenses){
@@ -94,22 +96,11 @@ ForEach($license in $licenses)
         }elseif ($usedLicenses -lt $totalLicenses){
             $license_usage = "OK: $usedLicenses out of $totalLicenses licenses are claimed."
             $output_jobs_success_counter++
-=======
-        $exitcode = $EXIT_OK
-        if($usedLicenses -ge $totalLicenses){
-            $license_usage = "Warning: $usedLicenses out of $totalLicenses licenses are claimed."
-            $exitcode = $EXIT_WARNING
-        }elseif ($usedLicenses -lt $totalLicenses){
-            $license_usage = "OK: $usedLicenses out of $totalLicenses licenses are claimed."
->>>>>>> 164938a3f1a6e8096a16b322e4de22fdcbf823cd
             $exitcode = $EXIT_OK
         }
     }else{
         $license_status = "Critical: License is $licensestatus"
-<<<<<<< HEAD
         $output_jobs_failed_counter++
-=======
->>>>>>> 164938a3f1a6e8096a16b322e4de22fdcbf823cd
     }
 }
     <# Monitoring license lifetime.
@@ -119,24 +110,16 @@ ForEach($license in $licenses)
     #>
 
     if ($licenseLifetime) {
-<<<<<<< HEAD
-        $support_status = "OK: Support is valid"
+        $support_status = 'OK: Support is valid'
         $output_jobs_success_counter++
         $exitcode = $EXIT_OK
     }else{
-        $support_status = "Critical: Support is invalid (no license found)"
+        $support_status = 'Critical: Support is invalid (no license found)'
         $output_jobs_failed_counter++
-=======
-        $support_status = "Support is valid"
-        $exitcode = $EXIT_OK
-    }else{
-        $support_status = "Support is invalid (no license found)"
->>>>>>> 164938a3f1a6e8096a16b322e4de22fdcbf823cd
         $exitcode = $EXIT_CRITICAL
     }
 
     if($output_jobs_failed_counter -gt 0){
-<<<<<<< HEAD
         write-Host "$successful_jobs $license_status, $license_usage $support_status`n$critical_jobs $warning_jobs"
         $exitcode = $EXIT_CRITICAL
     }
@@ -144,22 +127,9 @@ ForEach($license in $licenses)
         write-Host "$successful_jobs $license_status, $license_usage $support_status`n$warning_jobs"
         $exitcode = $EXIT_WARNING
     }
-    elseif ($output_jobs_success_counter -gt 0) {
-        Write-Host "$successful_jobs $licensestatus, $license_usage $support_status"
-        $exitcode = $EXIT_OK
-    }
     else{
         write-Host "$successful_jobs $license_status, $license_usage $support_status"
         $exitcode = $EXIT_OK
-=======
-        write-Host "Failed Jobs founds. $successful_jobs $license_status, $license_usage $support_status`n$critical_jobs $warning_jobs"
-    }
-    elseif($output_jobs_warning_counter -gt 0){
-        write-Host "Failed Jobs founds. $successful_jobs $license_status, $license_usage $support_status`n$warning_jobs"
-    }
-    else{
-        write-Host "$successful_jobs $license_status, $license_usage $support_status"
->>>>>>> 164938a3f1a6e8096a16b322e4de22fdcbf823cd
     }
 
     exit $exitcode
